140da491151db30200d081425c10b12e749a2133
pro200 <pro200@gmail.com>
2026-02-04 13:29:46 +0900

    update

--- datetime_test.go
+++ datetime_test.go
@@ -14,7 +14,7 @@ func TestDatetime(t *testing.T) {
 	}
 
 	str, err := utils.ParseTimeFormat("20251029122304", "%Y-%m-%d %H:%M:%S")
-	fmt.Println("->", str)
+	fmt.Println("-->", str)
 	if err != nil || str != "2025-10-29 12:23:04" {
 		t.Error("Wrong result")
 	}
